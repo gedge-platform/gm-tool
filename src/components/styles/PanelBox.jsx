caa4c40c97dea74eec31ebd9927826c260cba521
souvenir718 <mongkey0@naver.com>
2022-03-22 16:17:22 +0900

    [0322] 팝업 UI 적용

--- src/components/styles/PanelBox.jsx
+++ src/components/styles/PanelBox.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
-import theme from '@/styles/theme';
+import React from "react";
+import styled from "styled-components";
+import theme from "@/styles/theme";
 
 const PanelBox = styled.div`
   position: relative;
-  background-color: #fff;
+  background-color: #27324e;
   border-top: 1px solid ${theme.colors.defaultDark};
-  border-bottom: 1px solid #858f9c;
+  border-bottom: 1px solid #232f47;
   padding: 0 1px;
 
-  &::before, &::after {
-    content: '';
+  &::before,
+  &::after {
+    content: "";
     position: absolute;
     top: 0;
     bottom: 0;
   }
   &::before {
     left: 0;
-    border-left: 1px solid #c5cad0
+    border-left: 1px solid #232f47;
   }
   &::after {
     right: 0;
-    border-right: 1px solid #c5cad0
+    border-right: 1px solid #232f47;
   }
   .panelTitBar {
     display: flex;
@@ -29,19 +30,21 @@ const PanelBox = styled.div`
     align-items: center;
     height: 47px;
     padding: 0 8px;
-    border: 1px solid #fff;
-    border-bottom-color: ${theme.colors.defaultBorder};
+    border: 1px solid #2f3855;
+    border-bottom-color: #232f47;
     background-color: ${theme.colors.panelTit};
     .tit {
       padding-left: 12px;
       color: #071e3f;
       font-size: 13px;
-      strong { color: #0090ff }
+      strong {
+        color: #0090ff;
+      }
     }
     .date {
-      color: #a1a7af;
+      color: #fff;
       padding: 0 8px;
     }
   }
 `;
-export { PanelBox }
+export { PanelBox };
