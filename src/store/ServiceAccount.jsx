2565750539cb13cd3b9a1d0755dfa0d420712c5b
yjy <jyyu@innogrid.com>
2023-04-03 15:07:31 +0900

    [230403] pagination merge 완료

--- src/store/ServiceAccount.jsx
+++ src/store/ServiceAccount.jsx
@@ -35,8 +35,8 @@ class ServiceAccount {
     runInAction(() => {
       this.viewList = null;
       this.currentPage = 1;
-    })
-  }
+    });
+  };
 
   goPrevPage = () => {
     runInAction(() => {
@@ -128,23 +128,27 @@ class ServiceAccount {
   paginationList = () => {
     runInAction(() => {
       if (this.serviceAccountList !== null) {
-        this.viewList =  this.serviceAccountList.slice((this.currentPage-1)*10, this.currentPage*10);
+        this.viewList = this.serviceAccountList.slice(
+          (this.currentPage - 1) * 10,
+          this.currentPage * 10
+        );
       }
-    })
-  }
+    });
+  };
 
   loadServiceAccountList = async () => {
     await axios
       .get(`${SERVER_URL}/serviceaccounts`)
-      .then(({ data: { data } }) => {
+      .then((res) => {
         runInAction(() => {
-          this.serviceAccountList = data;
-          this.serviceAccountDetail = data[0];
-          this.totalElements = data.length;
+          this.serviceAccountList = res.data.data;
+          this.serviceAccountDetail = this.serviceAccountList[0];
+          this.totalElements = this.serviceAccountList.length;
+          this.totalPages = Math.ceil(this.serviceAccountList.length / 10);
         });
       })
       .then(() => {
-        this.convertList(this.serviceAccountList, this.setServiceAccountList);
+        this.paginationList();
       })
       .then(() => {
         this.loadServiceAccountTabList(
@@ -167,7 +171,7 @@ class ServiceAccount {
           if (this.serviceAccountList.length !== 0) {
             this.serviceAccountDetail = this.serviceAccountList[0];
             this.totalElements = this.serviceAccountList.length;
-            this.totalPages = Math.ceil(this.serviceAccountList.length/10); 
+            this.totalPages = Math.ceil(this.serviceAccountList.length / 10);
           } else {
             this.serviceAccountList = [];
           }
