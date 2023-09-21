8638c96a8d9923b4a7e8059ab599423fa79a3add
Hanseul-C <86642982+Hanseul-C@users.noreply.github.com>
2023-09-21 15:14:41 +0900

    [230921] Create Tamplate step1 type 선택 완료

--- src/pages/Gedge/Service/Project/Workload/Tablist/CreateTamplateStepOne.jsx
+++ src/pages/Gedge/Service/Project/Workload/Tablist/CreateTamplateStepOne.jsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 import { observer } from "mobx-react";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
-const Button = styled.button`
-  background-color: #fff;
-  border: 1px solid black;
-  color: black;
-  padding: 10px 35px;
-  margin-right: 10px;
-  border-radius: 4px;
-  /* box-shadow: 0 8px 16px 0 rgb(35 45 65 / 28%); */
-`;
+// const Button = styled.button`
+//   background-color: #fff;
+//   border: 1px solid black;
+//   color: black;
+//   padding: 10px 35px;
+//   margin-right: 10px;
+//   border-radius: 4px;
+//   /* box-shadow: 0 8px 16px 0 rgb(35 45 65 / 28%); */
+// `;
 
 const DeleteButton = styled.button`
   margin: 0px 0px 0px 3px;
@@ -63,23 +65,90 @@ const Table = styled.table`
   }
 `;
 
+const ImgButton = styled.button`
+  background-image: url(/images/resource/nginx.png);
+  border: black;
+`;
+
 const CreateTamplateStepOne = observer((props) => {
+  const [logo, setLogo] = useState("");
+
+  const onClickLogo = (e) => {
+    setLogo(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
-    <div className="step-container">
-      <div className="signup-step">
-        <div className="step current">
-          <span>기본 정보</span>
-        </div>
-        <div className="arr"></div>
-        <div className="step">
-          <span>고급 설정</span>
-        </div>
-        <div className="arr"></div>
-        <div className="step">
-          <span>설정 검토</span>
+    <>
+      <div className="step-container">
+        <div className="signup-step">
+          <div className="step current">
+            <span>기본 정보</span>
+          </div>
+          <div className="arr"></div>
+          <div className="step">
+            <span>고급 설정</span>
+          </div>
+          <div className="arr"></div>
+          <div className="step">
+            <span>설정 검토</span>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        style={{
+          width: "600px",
+          borderTop: "1px solid",
+          height: "300px",
+        }}
+      >
+        <div style={{ display: "flex", margin: "5px 5px 5px 5px" }}>
+          {/* <input
+            type="image"
+            src="images/resource/nginxLogo.png"
+            value="nginx"
+            width="150px"
+            onClick={onClickLogo}
+          />
+          <input
+            type="image"
+            src="images/resource/mysqlLogo.png"
+            value="mysql"
+            width="150px"
+            onClick={onClickLogo}
+          /> */}
+          <Button
+            onClick={onClickLogo}
+            value="nginx"
+            variant="outlined"
+            style={{ margin: "5px 5px 5px 5px" }}
+          >
+            {/* <img
+              src="images/resource/nginx.png"
+              alt="nginx"
+              style={{ display: "block" }}
+              width={50}
+              height={50}
+            /> */}
+            NGINX
+          </Button>
+          <Button
+            onClick={onClickLogo}
+            value="mysql"
+            variant="outlined"
+            style={{ margin: "5px 5px 5px 5px" }}
+          >
+            {/* <img
+              src="images/resource/mysql.png"
+              alt="mysql"
+              width={50}
+              // style={{ display: "block" }}
+            /> */}
+            MySQL
+          </Button>
+        </div>
+      </div>
+    </>
   );
 });
 
